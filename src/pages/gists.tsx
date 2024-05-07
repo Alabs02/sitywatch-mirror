fbfc79766ab23bc2318015d932e0b99dd1ffe39e
iykeslim <iykeslim01@gmail.com>
2024-05-07 20:13:20 +0100

    gists layout changes

--- src/pages/gists.tsx
+++ src/pages/gists.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Gists = () => {
   return (
-    <div className="w-full h-full px-14 border border-[red] p-2 grid gap-y-4">
+    <div className="w-full h-full border border-[red] px-5 lg:p-6 grid gap-y-4 ">
       <section className="grid grid-cols-12 gap-x-14">
         <div className="grid col-span-8 border border-[red] p-14 bg-gray-300 shadow-lg rounded-md">
           <div className="bg-gray-300 shadow-inner shadow-gray-400/75 py-6 px-2">
