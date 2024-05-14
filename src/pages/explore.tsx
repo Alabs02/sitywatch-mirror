0e06ce4d56f99128f606f1127311efcc149869c9
iykeslim <iykeslim01@gmail.com>
2024-05-14 12:10:08 +0100

    explore content and accessories

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+// import Image from "next/image";
 
-const Explore = () => {
+// LAYOUTS
+import { AppLayout } from "@/layouts"
+import { FC, ReactNode } from "react"
+
+const Explore: FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <div>Explore</div>
+    <div className="grid w-full h-full pr-5 ">
+      <div className="w-full h-full grid shadow-inner shadow-gray-400/75 border rounded-t-[32px] px-5 lg:p-6">
+        <section className="h-full w-full grid grid-cols-12">
+          
+        </section>
+        <section className="mt-8">
+          
+        </section>
+        <section className="mt-8">
+          <h1 className="font-bold">
+            Bottom of the top
+          </h1>
+          <div className="grid grid-cols-2 gap-x-2 ">
+            <div className="grid col-span-1 border border-[red] p-24"></div>
+            <div className="grid col-span-1 border border-[red] p-24"></div>
+          </div>
+        </section>
+      </div>
+    </div>
   )
 }
 
