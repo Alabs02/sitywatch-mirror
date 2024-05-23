f0b1a4df7ddbb9defe433206aac5bd4b7d93c03e
iykeslim <iykeslim01@gmail.com>
2024-05-23 15:36:53 +0100

    still gists on bookmarks

--- src/components/contents/gists/Gists.tsx
+++ src/components/contents/gists/Gists.tsx
@@ -8,6 +8,7 @@ interface Bookmarks {
   handle: string
   university: string
   role: string
+  time: string
   gist: string
   icons: string[]
   gif: string
@@ -24,25 +25,27 @@ const Gists: React.FC = () => {
     <div>
       {bookmarks.map((bookmark) => (
         <div
-          className="flex space-y-4 w-full h-full mt-4 ml-2"
+          className="flex flex-col space-y-4 w-full h-full mt-4 bg-neutral-400 p-1 rounded-md overflow-y-auto"
           key={bookmark.id}
         >
           <div className="flex justify-between w-full h-full items-center">
-            <div className="flex">
+            <div className="flex items-center">
               <img
                 src={bookmark.avatarSrc}
                 alt={bookmark.role}
-                className="h-12 w-12 object-cover rounded-full ring-gradient mr-2"
+                className="h-16 w-16 object-cover rounded-full ring-gradient mr-2"
               />
-              <div>
+              <div className='text-sm'>
                 <div className="text-green-600">{bookmark.handle}</div>
                 <div className="flex items-center">
-                  <div className="mx-2">{bookmark.university}</div>
+                  <div className="mr">{bookmark.university}</div>
                   <div className='flex items-center'>
                     <span className="h-1 w-1 bg-slate-700 mx-1 rounded-full"></span>
                     <div>{bookmark.role}</div>
                   </div>
                 </div>
+                <h4 className='text-[12px]'>{bookmark.time}</h4>
+                {/* <span>span> */}
               </div>
             </div>
             <div>
@@ -51,6 +54,12 @@ const Gists: React.FC = () => {
               </span>
             </div>
           </div>
+          <section>
+            <span>{bookmark.gist}</span>
+            <div>
+              <img src={bookmark.gif} alt={bookmark.role} />
+            </div>
+          </section>
         </div>
       ))}
     </div>
