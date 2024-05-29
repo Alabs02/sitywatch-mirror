68eb99e52f0cb6a963aac22e03a91cc36c79d2e2
iykeslim <iykeslim01@gmail.com>
2024-05-29 23:35:07 +0100

    tourneys layout

--- src/pages/tourneys.tsx
+++ src/pages/tourneys.tsx
@@ -1,16 +1,65 @@
 import React, { useEffect } from "react"
 import { useRouter } from "next/router"
 
+const LeftSide: React.FC = () => {
+  return (
+    <div className="flex-1 overflow-y-auto h-screen p-2">
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+      <h1>left</h1>
+    </div>
+  )
+}
+
+const RightSide: React.FC = () => {
+  return (
+    <div className="flex-1 bg-gray-200">
+      {/* Content for the right side */}
+      {/* This side will remain static */}
+    </div>
+  )
+}
+
 const Tourneys: React.FC = () => {
   const router = useRouter()
-  const { cardId } = router.query
 
   useEffect(() => {
     const query = { collapsed: true }
     router.push({ pathname: "/tourneys", query })
   }, [])
 
-  return <div>Tourneys</div>
+  return (
+    <div className="flex">
+      <LeftSide />
+      <RightSide />
+    </div>
+  )
 }
 
 export default Tourneys
