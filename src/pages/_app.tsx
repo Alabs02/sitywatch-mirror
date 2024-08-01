06e5226d6693cce037857fd9f3ed5625483fafb8
iykeslim <iykeslim01@gmail.com>
2024-08-01 12:35:29 +0100

    favicon changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,21 @@ import { Provider } from "react-redux"
 import store from "@/store/store" // Assuming your Redux store is in this location
 import { AppProps } from "next/app"
 import { Fragment } from "react"
+import Head from "next/head"
 
 // Import your global styles here
 import "@/styles/globals.scss"
 import "animate.css"
 import { Layout } from "@/layouts"
 
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Fragment>
+        <Head>
+          <title>Scout City</title>
+          <link rel="icon" href="/sclogo-light.png" type="image/svg+xml" />
+        </Head>
         <Layout>
           <Component {...pageProps} />
         </Layout>
