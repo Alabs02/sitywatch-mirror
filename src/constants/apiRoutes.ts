f2de839c1b265657bfb866b2a54d6f939a5856d7
iykeslim <iykeslim01@gmail.com>
2024-09-28 18:38:05 +0100

    sitadel auth changes

--- src/constants/apiRoutes.ts
+++ src/constants/apiRoutes.ts
@@ -3,7 +3,7 @@ export const baseURI = "https://sitywatch-backend.onrender.com/api/v1/"
 export const apiRoutes = {
   SITIZENS_SIGN_UP: "/sitizens",
   VERIFY_EMAIL: "/auth/verifyEmail",
-  SITADELS_SIGN_UP: "/sitadels",
+  SITADELS_SIGN_UP: "sitadels",
   SIGN_IN: "/auth/signin",
   SIGN_OUT: "/auth/signout",
 }
